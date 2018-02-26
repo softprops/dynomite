b2f26535db02a3fe736b2f01bd559901b6235057
softprops <d.tangren@gmail.com>
2018-02-25 22:41:51 -0500

    error tests

--- dynomite/src/error.rs
+++ dynomite/src/error.rs
@@ -5,13 +5,38 @@
 pub enum AttributeError {
   /// Will be returned if an AttributeValue is present, and is of the expected
   /// type but its contents are not well-formatted
-  #[fail(display = "Invalid type")]
+  #[fail(display = "Invalid format")]
   InvalidFormat,
   /// Will be returned if provided AttributeValue is not of the expected type
-  #[fail(display = "Missing value")]
+  #[fail(display = "Invalid type")]
   InvalidType,
   /// Will be returned if provided attributes does not included an
   /// expected named value
   #[fail(display = "Missing field {}", name)]
   MissingField { name: String },
+}
+
+#[cfg(test)]
+mod tests {
+  use super::AttributeError;
+  #[test]
+  fn invalid_format_derives() {
+    assert_eq!(
+      "Invalid format",
+      format!("{}", AttributeError::InvalidFormat)
+    )
+  }
+
+  #[test]
+  fn invalid_type_derives() {
+    assert_eq!("Invalid type", format!("{}", AttributeError::InvalidType))
+  }
+
+  #[test]
+  fn missing_field_derives() {
+    assert_eq!(
+      "Missing field foo",
+      format!("{}", AttributeError::MissingField { name: "foo".into() })
+    )
+  }
 }
