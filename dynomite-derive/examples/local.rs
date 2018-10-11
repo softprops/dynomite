9407ca34602f8c9977d0ad493b0e8548a338436f
softprops <d.tangren@gmail.com>
2018-10-11 19:25:18 -0400

    rustfmt

--- dynomite-derive/examples/local.rs
+++ dynomite-derive/examples/local.rs
@@ -10,8 +10,8 @@ extern crate uuid;
 use std::sync::Arc;
 
 use dynamodb::{
-  AttributeDefinition, CreateTableInput, DynamoDb, DynamoDbClient, GetItemInput, KeySchemaElement,
-  ProvisionedThroughput, PutItemInput, ScanInput,
+    AttributeDefinition, CreateTableInput, DynamoDb, DynamoDbClient, GetItemInput,
+    KeySchemaElement, ProvisionedThroughput, PutItemInput, ScanInput,
 };
 // dynomite re-exports `rusoto_dynamodb` for convenience
 use dynomite::dynamodb;
@@ -28,79 +28,80 @@ use uuid::Uuid;
 
 #[derive(Item, Debug, Clone)]
 pub struct Book {
-  #[hash]
-  id: Uuid,
-  title: String,
+    #[hash]
+    id: Uuid,
+    title: String,
 }
 
 // this will create a rust book shelf in your aws account!
 fn main() {
-  let mut rt = Runtime::new().expect("failed to initialize futures runtime");
-  // create rusoto client
-  let client = Arc::new(DynamoDbClient::new(Region::Custom {
-    name: "us-east-1".into(),
-    endpoint: "http://localhost:8000".into(),
-  }));
+    let mut rt = Runtime::new().expect("failed to initialize futures runtime");
+    // create rusoto client
+    let client = Arc::new(DynamoDbClient::new(Region::Custom {
+        name: "us-east-1".into(),
+        endpoint: "http://localhost:8000".into(),
+    }));
 
-  // create a book table with a single string (S) primary key.
-  // if this table does not already exists
-  // this may take a second or two to provision.
-  // it will fail if this table already exists but that's okay,
-  // this is just an example :)
-  let table_name = "books".to_string();
-  let _ = rt.block_on(client.create_table(CreateTableInput {
-    table_name: table_name.clone(),
-    key_schema: vec![KeySchemaElement {
-      attribute_name: "id".into(),
-      key_type: "HASH".into(),
-    }],
-    attribute_definitions: vec![AttributeDefinition {
-      attribute_name: "id".into(),
-      attribute_type: "S".into(),
-    }],
-    provisioned_throughput: ProvisionedThroughput {
-      read_capacity_units: 1,
-      write_capacity_units: 1,
-    },
-    ..CreateTableInput::default()
-  }));
-
-  let book = Book {
-    id: Uuid::new_v4(),
-    title: "rust".into(),
-  };
-
-  // print the key for this book
-  // requires bringing `dynomite::Item` into scope
-  println!("book.key() {:#?}", book.key());
-
-  // add a book to the shelf
-  println!(
-    "put_item() result {:#?}",
-    rt.block_on(client.put_item(PutItemInput {
-      table_name: table_name.clone(),
-      item: book.clone().into(), // <= convert book into it's attribute map representation
-      ..PutItemInput::default()
-    }))
-  );
-
-  println!(
-    "put_item() result {:#?}",
-    rt.block_on(
-      client.put_item(PutItemInput {
+    // create a book table with a single string (S) primary key.
+    // if this table does not already exists
+    // this may take a second or two to provision.
+    // it will fail if this table already exists but that's okay,
+    // this is just an example :)
+    let table_name = "books".to_string();
+    let _ = rt.block_on(client.create_table(CreateTableInput {
         table_name: table_name.clone(),
-        // convert book into it's attribute map representation
-        item: Book {
-          id: Uuid::new_v4(),
-          title: "rust and beyond".into(),
-        }.into(),
-        ..PutItemInput::default()
-      })
-    )
-  );
+        key_schema: vec![KeySchemaElement {
+            attribute_name: "id".into(),
+            key_type: "HASH".into(),
+        }],
+        attribute_definitions: vec![AttributeDefinition {
+            attribute_name: "id".into(),
+            attribute_type: "S".into(),
+        }],
+        provisioned_throughput: ProvisionedThroughput {
+            read_capacity_units: 1,
+            write_capacity_units: 1,
+        },
+        ..CreateTableInput::default()
+    }));
 
-  // scan through all pages of results in the books table for books who's title is "rust"
-  println!(
+    let book = Book {
+        id: Uuid::new_v4(),
+        title: "rust".into(),
+    };
+
+    // print the key for this book
+    // requires bringing `dynomite::Item` into scope
+    println!("book.key() {:#?}", book.key());
+
+    // add a book to the shelf
+    println!(
+        "put_item() result {:#?}",
+        rt.block_on(client.put_item(PutItemInput {
+            table_name: table_name.clone(),
+            item: book.clone().into(), // <= convert book into it's attribute map representation
+            ..PutItemInput::default()
+        }))
+    );
+
+    println!(
+        "put_item() result {:#?}",
+        rt.block_on(
+            client.put_item(PutItemInput {
+                table_name: table_name.clone(),
+                // convert book into it's attribute map representation
+                item: Book {
+                    id: Uuid::new_v4(),
+                    title: "rust and beyond".into(),
+                }
+                .into(),
+                ..PutItemInput::default()
+            })
+        )
+    );
+
+    // scan through all pages of results in the books table for books who's title is "rust"
+    println!(
         "scan result {:#?}",
         rt.block_on(
             client
@@ -118,8 +119,8 @@ fn main() {
         ),
     );
 
-  // get the "rust' book by the Book type's generated key
-  println!(
+    // get the "rust' book by the Book type's generated key
+    println!(
         "get_item() result {:#?}",
         rt.block_on(
             client
