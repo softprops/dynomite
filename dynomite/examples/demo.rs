2294f1a8dac3a4cd662216feb74d55c6410c092c
James Erwin <jam3se@gmail.com>
2019-05-02 20:14:08 +0900

    Added rustls feature to enable using the rustls crate for dependancy rusoto_core

--- dynomite/examples/demo.rs
+++ dynomite/examples/demo.rs
@@ -8,7 +8,13 @@ use dynomite::{
     DynamoDbExt, FromAttributes, Item, Retries,
 };
 use futures::{Future, Stream};
-use rusoto_core::Region;
+
+#[cfg(feature = "default")]
+use rusoto_core_default::Region;
+
+#[cfg(feature = "rustls")]
+use rusoto_core_rustls::Region;
+
 use tokio::runtime::Runtime;
 use uuid::Uuid;
 
