7bff503a50eb8e5b82b47d41f35c5e92eb2aac83
softprops <softprops@users.noreply.github.com>
2020-07-13 15:58:30 +0000

    deploy: 62be32aeddb758fa0bc27794c6c9ea158db4678f

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["dynomite"] = {"name":"","files":["error.rs","ext.rs","lib.rs","retry.rs"]};
-sourcesIndex["dynomite_derive"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["dynomite_derive"] = {"name":"","files":["attr.rs","lib.rs"]};
 createSourceSidebar();
